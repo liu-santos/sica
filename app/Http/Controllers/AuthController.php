f9a7fa03e32426407796f7de0a2bb7968da487fb
Lilian <lilian.santosilva@gmail.com>
2025-07-16 21:16:32 -0300

    login inicio

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        return view('login');
+    }
+
+
+    public function loginSubmit(Request $request)
+    {
+        echo $request->input('text_username');
+        echo $request->input('text_password');
+    }
+
+
+    public function logout()
+    {
+        echo'logout';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/main',[MainController::class,'index']); 
+Route::get('/login', [AuthController::class, 'login'])->name('login'); 
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit'])->name('loginSubmit');    
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout'); 
