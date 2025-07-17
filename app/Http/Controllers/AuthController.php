6b3d7f1086df237260b9045506856b2a04012a1f
Lilian <lilian.santosilva@gmail.com>
2025-07-16 21:27:33 -0300

    aula 44

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,8 +14,18 @@ class AuthController extends Controller
 
     public function loginSubmit(Request $request)
     {
-        echo $request->input('text_username');
-        echo $request->input('text_password');
+       //form validacao
+
+       $request->validate([
+            'text_username' => 'required|string|max:5|min:3',
+            'text_password' => 'required|string|min:6',
+        ]);
+
+        $username = $request->input('text_username');
+        $userpassword = $request->input('text_password');
+   
+        echo '0k';
+   //aula 44
     }
 
 
